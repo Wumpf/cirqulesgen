821b31047814e75b194da467a06f681a38db5995
Andreas Reich <r_andreas2@web.de>
2022-07-03 20:53:04 +0200

    less matrix ops :)

--- cirqulesgen.py
+++ cirqulesgen.py
@@ -8,18 +8,17 @@ w, h = 1024, 768
 # data = np.zeros((h, w, 3), dtype=np.uint8)
 # data[0:w, 0:h] = [255, 255, 255]
 
-comps_x = np.tile(np.arange(w), h)
-comps_y = np.arange(h).repeat(w)
+pixel_coordinates = np.stack([np.arange(h).repeat(w), np.tile(np.arange(w), h)])
 
 # draw random circles
 rng = default_rng(28346)
 circle_count = 64
 radius_range = (40, 150)
 circles_radius_sq = rng.triangular(left=radius_range[0], mode=radius_range[0], right=radius_range[1], size=circle_count) ** 2
-circles_position = rng.uniform(size=(2, circle_count)) * [[w], [h]]
+circles_position = rng.uniform(size=(circle_count, 2)) * [w, h]
 
-dist_circles_sq = ((np.broadcast_to(comps_x[None, :], (circle_count,) + comps_x.shape) - circles_position[0, :, None]) ** 2 + 
-                   (np.broadcast_to(comps_y[None, :], (circle_count,) + comps_y.shape) - circles_position[1, :, None]) ** 2)
+dist_circles_sq = np.sum((np.broadcast_to(pixel_coordinates[None, :, :], (circle_count,) + pixel_coordinates.shape) - circles_position[:, :, None]) ** 2, axis=1)
+
 # boolean matrix - true for each pixel (each colum is a different pixel) if it is contained in a circle (each row for a different circle)
 circle_pix = dist_circles_sq < circles_radius_sq[:, None]
 
@@ -27,7 +26,6 @@ circle_pix = dist_circles_sq < circles_radius_sq[:, None]
 perpix_area_index = np.packbits(circle_pix, axis=0)
 byte_factors = 256 ** np.arange(8, dtype=np.int64)
 perpix_area_index = (perpix_area_index * byte_factors[:perpix_area_index.shape[0], None]).sum(axis=0)
-assert(perpix_area_index.shape == comps_x.shape)
 
 # determine the color for each area
 unique_area_indices = np.unique(perpix_area_index)
