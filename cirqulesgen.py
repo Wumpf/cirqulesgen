0a396b1e2023f186ad5f3b831f3d2b890a31fd95
Andreas Reich <r_andreas2@web.de>
2022-07-23 12:45:39 +0200

    config class

--- cirqulesgen.py
+++ cirqulesgen.py
@@ -6,14 +6,15 @@ import numpy as np
 import cv2
 from numpy.random import default_rng
 
-image_path = 'couple.png'
-circle_count = 200
-radius_range = (8, 80)
-local_variance_window_size = radius_range[1] + 1
-non_background_pdf_bias = 20
-background_color = np.array([255, 255, 255], dtype=np.ubyte)
+class Config:
+    image_path = 'couple.png'
+    circle_count = 200
+    radius_range = (8, 80)
+    local_variance_window_size = radius_range[1] + 1
+    non_background_pdf_bias = 20
+    background_color = np.array([255, 255, 255], dtype=np.ubyte)
 
-def gen_random_circles(flat_pixel_coord, circle_pdf, rng: Generator):
+def gen_random_circles(flat_pixel_coord, circle_pdf, rng: Generator, radius_range: tuple, circle_count: int):
     circles_radius_sq = rng.triangular(left=radius_range[0], mode=radius_range[0], right=radius_range[1], size=circle_count) ** 2
     circles_position = rng.choice(flat_pixel_coord, circle_count, p=np.reshape(circle_pdf, flat_pixel_coord.shape[1]), replace=False, axis=1)
     #circles_position = rng.uniform(size=(circle_count, 2)) * image_size
@@ -23,15 +24,15 @@ def compute_area_indices(flat_pixel_coord, circles_radius_sq, circles_position):
     flat_pixel_coord_chunk_broadcast = np.broadcast_to(flat_pixel_coord[:, :, None], flat_pixel_coord.shape + (64,))
     perpix_area_index = np.zeros(flat_pixel_coord.shape[1], dtype=np.int64)
     byte_factors = 2 ** np.arange(64, dtype=np.int64)
-    for chunk_start in range(0, circle_count, 64):
-        chunk_end = min(chunk_start + 64, circle_count)
+    for chunk_start in range(0, circles_radius_sq.size, 64):
+        chunk_end = min(chunk_start + 64, circles_radius_sq.size)
         chunk = range(chunk_start, chunk_end)
         dist_circles_sq = np.sum((flat_pixel_coord_chunk_broadcast[:, :, :len(chunk)] - circles_position[:, None, chunk]) ** 2, axis=0)
         perpix_circle_hits = dist_circles_sq < circles_radius_sq[None, chunk]
         perpix_area_index ^= (perpix_circle_hits * byte_factors[None, :len(chunk)]).sum(axis=1)
     return perpix_area_index
 
-def fill_areas(perpix_area_index, reference_image_data):
+def fill_areas(perpix_area_index, reference_image_data, background_color):
     unique_area_indices = np.unique(perpix_area_index)
     output = np.tile(background_color, perpix_area_index.shape + (1,))
     for i in range(0, unique_area_indices.size):
@@ -62,20 +63,20 @@ def load_image(path):
     return np.reshape(np.array(reference_image.getdata()), (reference_image.size[1], reference_image.size[0], 3))
 
 if __name__ == '__main__':
-    reference_image = load_image(image_path)
+    reference_image = load_image(Config.image_path)
     h,w,_ = reference_image.shape
     reference_image_luminance = np.matmul(reference_image, [0.2126, 0.7152, 0.0722], dtype=np.float32)
     #Image.fromarray(np.reshape(reference_image_luminance, [h,w]).astype(np.ubyte), 'L').show()
-    windowed_var = compute_windowed_var(local_variance_window_size, reference_image_luminance)
+    windowed_var = compute_windowed_var(Config.local_variance_window_size, reference_image_luminance)
     circle_pdf = np.sqrt(windowed_var)
-    circle_pdf[(reference_image != background_color).all(axis=2)] += non_background_pdf_bias
+    circle_pdf[(reference_image != Config.background_color).all(axis=2)] += Config.non_background_pdf_bias
     Image.fromarray((circle_pdf).astype(np.ubyte), 'L').show()
     circle_pdf /= np.sum(circle_pdf)
 
     flat_pixel_coord = np.stack([np.tile(np.arange(w), h), np.arange(h).repeat(w)])
     rng = default_rng(52348)
 
-    circles_radius_sq, circles_position = gen_random_circles(flat_pixel_coord, circle_pdf, rng)
+    circles_radius_sq, circles_position = gen_random_circles(flat_pixel_coord, circle_pdf, rng, Config.radius_range, Config.circle_count)
     perpix_area_index = compute_area_indices(flat_pixel_coord, circles_radius_sq, circles_position)
-    querkle_picture = fill_areas(perpix_area_index, np.reshape(reference_image, (h * w, 3)))
+    querkle_picture = fill_areas(perpix_area_index, np.reshape(reference_image, (h * w, 3)), Config.background_color)
     save_and_show(np.reshape(querkle_picture, reference_image.shape))
